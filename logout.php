fcd5360e0493a29fc57a80ca86b42b69828d32d4
BOUBEE DE GRAMONT <BM325780>
2020-01-29 09:16:13 +0100

    logout:

--- logout.php
+++ logout.php
@@ -2,5 +2,22 @@
 session_start();
 $_SESSION = array();
 session_destroy();
-header("Location: login.php");
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Deconnexion</title>
+</head>
+<div align="center">
+<h2>Vous avez été deconnecté</h2>
+<p><a href="faq.php">Retourner a la FAQ</a></p>    
+</div>
+
+<body>
+    
+</body>
+</html>
+
