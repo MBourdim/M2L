53b5f48ea50e4bf5b0efd01185d9ef70b0fe23d3
BOUBEE DE GRAMONT <BM325780>
2020-01-29 08:16:32 +0100

    index.php

--- login.php
+++ login.php
@@ -1,22 +1,36 @@
 <?php
-session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=m2l', 'root', '');
 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=m2l', 'root', '');
-
-if(isset($_POST['formconnexion'])) {
-   $pseudoconnect = htmlspecialchars($_POST['pseudoconnect']);
-   $mdpconnect = sha1($_POST['mdpconnect']);
-   if(!empty($pseudoconnect) AND !empty($mdpconnect)) {
-      $requser = $bdd->prepare("SELECT * FROM user WHERE pseudo = ? AND mdp = ?");
-      $requser->execute(array($pseudoconnect, $mdpconnect));
-      $userexist = $requser->rowCount();
-      if($userexist == 1) {
-         $userinfo = $requser->fetch();
-         $_SESSION['id_user'] = $userinfo['id_user'];
-         $_SESSION['pseudo'] = $userinfo['pseudo'];
-         header("Location: faq.php?id_user=".$_SESSION['id_user']);
+if(isset($_POST['forminscription'])) {
+   $pseudo = htmlspecialchars($_POST['pseudo']);
+   $mail = htmlspecialchars($_POST['mail']);
+   $mail2 = htmlspecialchars($_POST['mail2']);
+   $mdp = sha1($_POST['mdp']);
+   $foot = htmlspecialchars($_POST['foot']);
+   if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp'])) {
+      $pseudolength = strlen($pseudo);
+      if($pseudolength <= 255) {
+         if($mail == $mail2) {
+            if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+               $reqmail = $bdd->prepare("SELECT * FROM user WHERE mail = ?");
+               $reqmail->execute(array($mail));
+               $mailexist = $reqmail->rowCount();
+               if($mailexist == 0) {
+                  $insertmbr = $bdd->prepare("INSERT INTO user(pseudo, mail, mdp) VALUES(?, ?, ?)");
+                  $insertmbr->execute(array($pseudo, $mail, $mdp));
+                  $erreur = "Votre compte a bien été créé ! <a href=\"login.php\">Me connecter</a>";
+                  
+               } else {
+                  $erreur = "Adresse mail déjà utilisée !";
+               }
+            } else {
+               $erreur = "Votre adresse mail n'est pas valide !";
+            }
+         } else {
+            $erreur = "Vos adresses mail ne correspondent pas !";
+         }
       } else {
-         $erreur = "Mauvais pseudo ou mot de passe !";
+         $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
       }
    } else {
       $erreur = "Tous les champs doivent être complétés !";
@@ -25,18 +39,36 @@ if(isset($_POST['formconnexion'])) {
 ?>
 <html>
    <head>
-      <title>Login</title>
+      <title></title>
       <meta charset="utf-8">
    </head>
    <body>
-      <div align="center">
-         <h2>Connexion</h2>
-         <br/><br />
+      
+         <h2>Inscription</h2>
+         <br /><br />
          <form method="POST" action="">
-            <input type="text" name="pseudoconnect" placeholder="Pseudo" />
-            <input type="password" name="mdpconnect" placeholder="Mot de passe" />
-            <br /><br />
-            <input type="submit" name="formconnexion" value="Se connecter !" />
+            <b>Pseudo</b>
+            <br>
+               <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" />
+            <br><br>
+            <b for="mail">Mail</b>
+            <br>
+                <input type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>" />
+            <br><br>
+            <b for="mail2">Confirmation du mail</b>
+            <br>
+                <input type="email" placeholder="Confirmez votre mail" id="mail2" name="mail2" value="<?php if(isset($mail2)) { echo $mail2; } ?>" />
+            <br><br>
+            <b for="mdp">Mot de passe</b>
+            <br>
+                <input type="password" placeholder="Votre mot de passe" id="mdp" name="mdp" />
+            <br><br>
+            <b for="ligue">Ligue</b>
+            <select name="Ligue" size="1">
+            <option type="text" id="foot" name="foot" value="<?php if(isset($foot)) { echo $foot; } ?>">Ligue de Football</option>
+            <option type="text" id="rugby" name="rugby" value="<?php if(isset($rugby)) { echo $rugby; } ?>">Ligue de Rugby</option>
+            <br>     
+            <input type="submit" name="forminscription" value="Je m'inscris" />
          </form>
          <?php
          if(isset($erreur)) {
