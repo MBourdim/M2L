a3ac7ed8e68945ddccea5b91a42d35c3b78ce5e3
BOUBEE DE GRAMONT <BM325780>
2020-02-05 11:06:17 +0100

    update

--- list.php
+++ list.php
@@ -6,31 +6,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css\tableaux.css">
+  <link rel="stylesheet" href="css/tableaux.css">
   <title>Liste des questions FAQ</title>
 </head>
 <body>
 <div class="outer-div">
     <div class="inner-div">
     <center><img class="imagecentre1" width="300" height="150" alt="imgc1" src="img/FAQfoot.png"></center>
-</div>
-<center>
-    <a href="faq.php"class="active">Accueil de la FAQ</a>
-    <a href="index.php">Maison des ligues</a>
-    <a href="football.php">Ligue de Football</a>
-    <a href="list.php">Liste des questions</a>
-    <a href="add.php">Ajouter une question</a>
-    <a href="login.php">Connexion</a>
-    <a href="logout.php">Déconnexion</a>
-    <a href="register.php">Inscription</a>
-</div>
+
 <ul>
-  <li><a href="faq.php"class="active">Accueil de la FAQ</a></li>
-  <li><a href="index.php">Maison des ligues</a></li>
-  <li><a href="football.php">Ligue de Football</a></li>
-  <li><a href="#about">About</a></li>
+  <li><a href="faq.php">Accueil de la FAQ</a></li>
+  <li><a href="list.php"class="active">Liste des questions</a></li>
+  <li><a href="add.php">Ajouter une question</a></li>
+  <li><a href="edit.php">Répondre à une question</a></li>
+  <li><a href="delete.php">Supprimer une question</a></li>
 </ul>
 <br><br>
+<center>
 <table>
       <tr>
         <th>N°</th>
@@ -40,9 +32,15 @@
         <th>Actions</th>
       </tr>
       <tr>
-        <td>sdf</td>
+        <td>1</td>
+        <td>sdfdu31</td>
+        <td>Qui aime Minecraft ?</td>
+        <td>Moi j'adore</td>
+        <td><img src="" alt=""></td>
     </table>
+</center>
+
 </body>    
-  </center>
+ 
     
 </html>
