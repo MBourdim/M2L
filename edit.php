af6e213d7ddd5bc03659748bc319c79ef28d8f81
BOUBEE DE GRAMONT <BM325780>
2020-02-05 11:26:54 +0100

    update

--- edit.php
+++ edit.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Repondre</title>
-    <link rel="stylesheet" type="text/css" href="css/tableaux.css">
+    <link rel="stylesheet" type="text/css" href="css/edit-liste.css">
 </head>
 <body>
 <div class="outer-div">
