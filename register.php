c08c645cb98617701e2b3179b15754bdfd6beedd
BOUBEE DE GRAMONT <BM325780>
2020-02-26 11:04:12 +0100

    update register

--- register.php
+++ register.php
@@ -4,81 +4,52 @@ include('./fonction.inc.php');
 $dbh = connexion(); // Connexion à la base de données
 ?>
 <?php
-if(isset($_POST['forminscription'])) {
-   $pseudo = htmlspecialchars($_POST['pseudo']);
-   $mail = htmlspecialchars($_POST['mail']);
-   $mail2 = htmlspecialchars($_POST['mail2']);
-   $mdp = sha1($_POST['mdp']);
 
-   if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp'])) {
-      $pseudolength = strlen($pseudo);
-      if($pseudolength <= 255) {
-         if($mail == $mail2) {
-            if(filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-               $reqmail = $bdd->prepare("SELECT * FROM user WHERE mail = ?");
-               $reqmail->execute(array($mail));
-               $mailexist = $reqmail->rowCount();
-               if($mailexist == 0) {
-                  $insertmbr = $bdd->prepare("INSERT INTO user(pseudo, mail, mdp) VALUES(?, ?, ?)");
-                  $insertmbr->execute(array($pseudo, $mail, $mdp));
-                  $erreur = "Votre compte a bien été créé ! <a href=\"login.php\">Me connecter</a>";
-                  
-               } else {
-                  $erreur = "Adresse mail déjà utilisée !";
-               }
-            } else {
-               $erreur = "Votre adresse mail n'est pas valide !";
-            }
-         } else {
-            $erreur = "Vos adresses mail ne correspondent pas !";
-         }
-      } else {
-         $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
-      }
-   } else {
-      $erreur = "Tous les champs doivent être complétés !";
-   }
+
+if(isset($_POST['submit'])){
+   if(!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['password'])){
+   $password = sha1($_POST['password']);
+   $insertsql = $dbh->prepare('INSERT INTO user(pseudo,mdp,mail,id_ligue) VALUES (?,?,?,?)');
+   $insertsql->execute(array($_POST['pseudo'],$_POST['mail'],$_POST['password'],$_POST['ligue']));
+if($insertsql->rowCount()==1){
+   echo "Super";
+}else{
+   echo "nul";
 }
+}
+}
+
+                        
+
 ?>
 <html>
    <head>
-      <title></title>
+      <title>Inscription</title>
       <meta charset="utf-8">
    </head>
    <body>
-    <div align="center">
+      <div align="center">
          <h2>Inscription</h2>
-         <br /><br />
-         <form method="POST" action="">
-            <b>Pseudo</b>
+         <br/><br/>
+         <form method="post" action="register.php" id="form1">
+            <p>Pseudo <br> <input type="text" name="pseudo" placeholder="Pseudo"/></p>
+            <p>Mail <br> <input type="text" name="mail" placeholder="Mail"/></p>
+            <p>Mot de passe <br><input type="password"name="password" placeholder="Mot de passe"/></p>
             <br>
-               <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" />
-            <br><br>
-            <b for="mail">Mail</b>
-            <br>
-                <input type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>" />
-            <br><br>
-            <b for="mail2">Confirmation du mail</b>
-            <br>
-                <input type="email" placeholder="Confirmez votre mail" id="mail2" name="mail2" value="<?php if(isset($mail2)) { echo $mail2; } ?>" />
-            <br><br>
-            <b for="mdp">Mot de passe</b>
-            <br>
-                <input type="password" placeholder="Votre mot de passe" id="mdp" name="mdp" />
-            <br><br>
-            <b for="ligue">Ligue</b>
-            <br>
-            <select name="Ligue" size="1">
-            <option type="text" id="foot" name="foot" value="1"<?php if(isset($foot)) { echo $foot; } ?> >Ligue de Football</option>
-            <option type="text" id="rugby" name="rugby" value="2"<?php if(isset($rugby)) { echo $rugby; } ?> >Ligue de Rugby</option>
-            <br>     
-            <input type="submit" name="forminscription" value="Je m'inscris" />
+            <p>
+            <select name="ligue">
+            <option value="1" selected>Football</option>
+            <option value="2" selected>Basket</option>
+            <option value="3" selected>Volley-ball</option>
+            <option value="4" selected>Handball</option>
+            </select>
+            </p>
+            <br/><br/>
+            <input type="submit" name="submit" value="Se connecter !" />
          </form>
          <?php
-         if(isset($erreur)) {
-            echo '<font color="red">'.$erreur."</font>";
-         }
          ?>
       </div>
+   
    </body>
 </html>
