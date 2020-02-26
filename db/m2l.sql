723d5fdd4e4c5d2f967a8b583f4e621a199153e3
BOUBEE DE GRAMONT <BM325780>
2020-02-26 11:42:31 +0100

    sql

--- db/m2l.sql
+++ db/m2l.sql
@@ -1,60 +1,201 @@
-CREATE DATABASE m2l CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 26 fév. 2020 à 11:41
+-- Version du serveur :  10.4.6-MariaDB
+-- Version de PHP :  7.3.9
 
-USE m2l;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE ligue
-(
-  id_ligue bigint(11) AUTO_INCREMENT,
-  lib_ligue varchar(50),
-  CONSTRAINT PK_ligue PRIMARY KEY (id_ligue)
-)ENGINE=InnoDB;
 
-CREATE TABLE usertype
-(
-  id_usertype bigint(11) AUTO_INCREMENT,
-  lib_usertype varchar(50),
-  description varchar(50),
-  CONSTRAINT PK_usertype PRIMARY KEY (id_usertype)
-)ENGINE=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `user`
-(
-  id_user bigint(11) AUTO_INCREMENT,
-  pseudo varchar(50) NOT NULL UNIQUE,
-  mdp varchar(100) NOT NULL,
-  mail varchar(50) NOT NULL UNIQUE,
-  id_usertype bigint(11),
-  id_ligue bigint(11),  
-  CONSTRAINT PK_user PRIMARY KEY (id_user),
-  CONSTRAINT FK_user_id_usertype FOREIGN KEY (id_usertype)
-  REFERENCES usertype (id_usertype),
-  CONSTRAINT FK_user_id_ligue FOREIGN KEY (id_ligue)
-  REFERENCES ligue (id_ligue)
-)ENGINE=InnoDB;
+--
+-- Base de données :  `m2l`
+--
+CREATE DATABASE IF NOT EXISTS `m2l` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `m2l`;
 
-CREATE TABLE faq
-(
-  id_faq bigint(11) AUTO_INCREMENT,
-  question text,
-  reponse text,
-  dat_question datetime,
-  dat_reponse datetime,
-  id_user bigint(11),  
-  CONSTRAINT PK_faq PRIMARY KEY (id_faq),
-  CONSTRAINT FK_faq_id_user FOREIGN KEY (id_user)
-  REFERENCES user (id_user)
-)ENGINE=InnoDB;
+-- --------------------------------------------------------
 
-INSERT INTO ligue 
-VALUES
- (1,"Ligue de football"),
- (2,"Ligue de basket"),
- (3,"Ligue de volley"), 
-(4,"Ligue de handball"), 
-(5,"Toutes les ligues");
+--
+-- Structure de la table `faq`
+--
 
-INSERT INTO usertype 
-VALUES 
-(1,"utilisateur","utilisateur de base"), 
-(2,"admin","administrateur de ligue"), 
-(3,"super-admin","administrateur de toutes les ligues");
+CREATE TABLE `faq` (
+  `id_faq` bigint(11) NOT NULL,
+  `question` text DEFAULT NULL,
+  `reponse` text DEFAULT NULL,
+  `dat_question` datetime DEFAULT NULL,
+  `dat_reponse` datetime DEFAULT NULL,
+  `id_user` bigint(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `faq`
+--
+
+INSERT INTO `faq` (`id_faq`, `question`, `reponse`, `dat_question`, `dat_reponse`, `id_user`) VALUES
+(1, 'A combien de joueurs se joue le football?', 'Il y a 11 joueurs par équipe.', '2020-01-22 00:00:00', '2020-01-29 12:30:00', 1),
+(2, 'Comment on fait une passe?', 'Avec son pied.', '2020-01-30 08:00:00', '2020-02-03 02:30:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ligue`
+--
+
+CREATE TABLE `ligue` (
+  `id_ligue` bigint(11) NOT NULL,
+  `lib_ligue` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `ligue`
+--
+
+INSERT INTO `ligue` (`id_ligue`, `lib_ligue`) VALUES
+(1, 'Ligue de football'),
+(2, 'Ligue de basket'),
+(3, 'Ligue de volley'),
+(4, 'Ligue de handball'),
+(5, 'Toutes les ligues');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` bigint(11) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `id_usertype` bigint(11) DEFAULT NULL,
+  `id_ligue` bigint(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `pseudo`, `mdp`, `mail`, `id_usertype`, `id_ligue`) VALUES
+(1, 'jeff', '$2y$10$2UeK58Jxm3Fyg3Z7KsJc5ehQjoEPYaGE5y97sERR3QWRAweeHCkhm', 'jeff@gmail.com', 1, 1),
+(2, 'dylan', '$2y$10$6yaGn3Ju0Ne50XPCnPdlautIsRAgkWbIowZD5ou51yk58bn6Oadsa', 'dylan@gmail.com', 2, 1),
+(3, 'georges', '$2y$10$4mEq79Krj.gMy9xSp5amZeuPZQHGhh24dchZfY8Lz6eQDu6WEiKxW', 'georges@gmail.com', 3, 5),
+(4, 'j', '0294cdcff3ed3c13dede749bd12a0c4ca222c7c7', 'jk', NULL, 4),
+(5, 'salut', 'effdb5f96a28acd2eb19dcb15d8f43af762bd0ae', 'g', NULL, 2),
+(7, 'jggg', '13f566a247aaf7dfd9cf0c8fa1ac5b140045f10a', 'jgggg', NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `usertype`
+--
+
+CREATE TABLE `usertype` (
+  `id_usertype` bigint(11) NOT NULL,
+  `lib_usertype` varchar(50) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `usertype`
+--
+
+INSERT INTO `usertype` (`id_usertype`, `lib_usertype`, `description`) VALUES
+(1, 'utilisateur', 'utilisateur de base'),
+(2, 'admin', 'administrateur de ligue'),
+(3, 'super-admin', 'administrateur de toutes les ligues');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `faq`
+--
+ALTER TABLE `faq`
+  ADD PRIMARY KEY (`id_faq`),
+  ADD KEY `FK_faq_id_user` (`id_user`);
+
+--
+-- Index pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  ADD PRIMARY KEY (`id_ligue`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `pseudo` (`pseudo`),
+  ADD UNIQUE KEY `mail` (`mail`),
+  ADD KEY `FK_user_id_usertype` (`id_usertype`),
+  ADD KEY `FK_user_id_ligue` (`id_ligue`);
+
+--
+-- Index pour la table `usertype`
+--
+ALTER TABLE `usertype`
+  ADD PRIMARY KEY (`id_usertype`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `faq`
+--
+ALTER TABLE `faq`
+  MODIFY `id_faq` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  MODIFY `id_ligue` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `usertype`
+--
+ALTER TABLE `usertype`
+  MODIFY `id_usertype` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `faq`
+--
+ALTER TABLE `faq`
+  ADD CONSTRAINT `FK_faq_id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `FK_user_id_ligue` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`),
+  ADD CONSTRAINT `FK_user_id_usertype` FOREIGN KEY (`id_usertype`) REFERENCES `usertype` (`id_usertype`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
